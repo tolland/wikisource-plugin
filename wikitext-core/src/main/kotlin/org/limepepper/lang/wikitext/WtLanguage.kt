d3a91640e7f69e6ed720ae4ad764a787a52ca1e5
Tom Hodder <tom@limepepper.co.uk>
2026-06-26 20:17:42 +0100

    move ui components to module

--- wikitext-core/src/main/kotlin/org/limepepper/lang/wikitext/WtLanguage.kt
+++ wikitext-core/src/main/kotlin/org/limepepper/lang/wikitext/WtLanguage.kt
@@ -2,8 +2,10 @@ package org.limepepper.lang.wikitext
 
 import com.intellij.lang.Language
 
-object WtLanguage : Language("Wikitext") {
+object WtLanguage : Language(
+    "Wikitext",
+    "text/x-wiki"
+) {
     override fun isCaseSensitive() = true
-
 
 }
